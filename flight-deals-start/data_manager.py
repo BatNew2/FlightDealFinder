61eb55f20ee417b4bfb86451a50027bdf7d4cf5e
BatNew2 <144895387+BatNew2@users.noreply.github.com>
2024-01-04 11:31:43 +0700

    Delete flight-deals-start directory

--- flight-deals-start/data_manager.py
+++
@@ -1,7 +0,0 @@
-import requests
-class DataManager:
-    def __init__(self):
-        self.response = requests.get("https://api.sheety.co/b3d5e792cbcb8a687f1186193cb7e04c/flightDeals/prices")
-        self.response.raise_for_status()
-        self.data = self.response.json()
-        self.data = [[city["from"], city["iataCode"], city["lowestPrice"], city['city']] for city in self.data["prices"]]
